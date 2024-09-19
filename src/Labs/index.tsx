0daebaedb8b3258352e858bfcbb9a20c327f3227
Rolin128 <li.yuchen@northeastern.edu>
2024-09-19 15:56:12 -0700

    a1-updates

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,15 +7,17 @@ import Lab3 from "./Lab3";
 export default function Labs() {
     return (
         <div id="wd-labs">
+            Yuchen Li <br />
+            Section: 01
             <h1>Labs</h1>
             <TOC />
+            
             <Routes>
                 <Route path="/" element={<Navigate to="Lab1" />} />
                 <Route path="Lab1" element={<Lab1 />} />
                 <Route path="Lab2" element={<Lab2 />} />
                 <Route path="Lab3" element={<Lab3 />} />
             </Routes>
-            < a id="wd-github" href="https://github.com/Rolin128/kanbas-react-web-app-cs5610-fa24"> github </a>
         </div>
     );
 }
