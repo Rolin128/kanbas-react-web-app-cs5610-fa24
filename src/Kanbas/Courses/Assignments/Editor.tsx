b33fe0e3f50ee81091109ab2df7be7dbbbf7ab3b
Rolin128 <li.yuchen@northeastern.edu>
2024-09-19 14:35:41 -0700

    A1-updates

---
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -0,0 +1,166 @@
+export default function AssignmentEditor() {
+    return (
+        <div id="wd-assignments-editor">
+            <label htmlFor="wd-name"><strong>Assignment Name</strong></label>
+            <br />
+            <br />
+            <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
+            <textarea
+                id="wd-description"
+
+                style={{ width: '140%', height: '150px' }} // 也可以通过style属性来调整
+            >
+                The assignment is available online Submit a link to the landing page of your Web application running on Netlify. The landing page should include the following: Your full name and section Links to each of the lab assignments Link to the Kanbas application Links to all relevant source code repositories The Kanbas application should include a link to navigate back to the landing page.
+            </textarea>
+            <br />
+            <br />
+
+            <table style={{ display: 'inline-block' }}>
+                <tr>
+                    <td align="right" valign="top">
+                        <label htmlFor="wd-points">Points</label>
+                    </td>
+                    <td>
+                        <input id="wd-points" value={100} />
+                    </td>
+                </tr>
+
+                <tr>
+                    <br></br>
+                </tr>
+
+                <tr>
+
+                    <td align="right" valign="top">
+                        <label htmlFor="wd-group">Assignment Group</label>
+                    </td>
+                    <td>
+                        <select id="wd-group">
+                            <option value="assignments">ASSIGNMENTS</option>
+                            {/* Add other groups if necessary */}
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <br></br>
+                </tr>
+                <tr>
+                    <td align="right" valign="top">
+                        <label htmlFor="wd-grade">Display Grade as</label>
+                    </td>
+                    <td>
+                        <select id="wd-grade">
+                            <option value="percentage">Percentage</option>
+                            <option value="points">Points</option>
+                            <option value="letter_grade">Letter Grade</option>
+                            <option value="gpa_scale">GPA Scale</option>
+                            <option value="complete_incomplete">Complete/Incomplete</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <br></br>
+                </tr>
+                <tr>
+                    <td align="right" valign="top">
+                        <label htmlFor="wd-submission">Submission Type</label>
+                    </td>
+                    <td>
+                        <select id="wd-submission">
+                            <option value="online">Online</option>
+                            {/* Add other submission types if necessary */}
+                        </select>
+
+                        <tr>
+                            <br></br>
+                            Online Entry Options
+                        </tr>
+                        <div>
+                            <input type="checkbox" id="text_entry" />
+                            <label htmlFor="text_entry">Text Entry</label><br />
+                            <input type="checkbox" id="website_url" />
+                            <label htmlFor="website_url">Website URL</label><br />
+                            <input type="checkbox" id="media_recordings" />
+                            <label htmlFor="media_recordings">Media Recordings</label><br />
+                            <input type="checkbox" id="student_annotation" />
+                            <label htmlFor="student_annotation">Student Annotation</label><br />
+                            <input type="checkbox" id="file_uploads" />
+                            <label htmlFor="file_uploads">File Uploads</label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <br></br>
+                </tr>
+
+                <tr>
+                    <td align="center" valign="top" colSpan={2}>
+                        <label htmlFor="wd-assign">Assign Assign to</label>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input id="wd-assign" value="Everyone" />
+                    </td>
+                </tr>
+                <tr>
+                    <br></br>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td align="left" valign="top">
+                        <label htmlFor="wd-due">Due</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input id="wd-due" type="date" value="2024-05-13" />
+                    </td>
+                </tr>
+                <tr>
+                    <br></br>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td align="right" valign="top" colSpan={2}>
+                        <table style={{ display: 'inline-block' }}>
+                            <tr>
+                                <td>
+                                    <label htmlFor="wd-available-from">Available from</label>
+
+                                </td>
+
+                                <td>
+                                    <label htmlFor="wd-until">Until</label>
+
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <input id="wd-available-from" type="date" value="2024-05-06" />
+
+                                </td>
+                                <td>
+                                    <input id="wd-until" type="date" value="2024-05-20" />
+
+                                </td>
+                            </tr>
+
+                        </table>
+
+                    </td>
+                </tr>
+            </table>
+
+            <br />
+            <div style={{ textAlign: 'right' }}>
+                <button style={{ marginRight: '10px' }}>Cancel</button>
+                <button>Save</button>
+            </div>
+        </div>
+    );
+}
---
+++ src/Kanbas/Dashboard.tsx
@@ -0,0 +1,118 @@
+import { Link } from "react-router-dom";
+export default function Dashboard() {
+    return (
+        <div id="wd-dashboard">
+            <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+            <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+            <div id="wd-dashboard-courses">
+                <div className="wd-dashboard-course">
+                    <Link className="wd-dashboard-course-link"
+                        to="/Kanbas/Courses/1234/Home">
+                        <img src="/images/reactjs.jpg" width={200} />
+                        <div>
+                            <h5>
+                                CS1234 React JS
+                            </h5>
+                            <p className="wd-dashboard-course-title">
+                                Full Stack software developer
+                            </p>
+                            <button> Go </button>
+                        </div>
+                    </Link>
+                </div>
+                <div className="wd-dashboard-course">
+                    <Link className="wd-dashboard-course-link"
+                        to="/Kanbas/Courses/1234/Home">
+                        <img src="/images/reactjs.jpg" width={200} />
+                        <div>
+                            <h5>
+                                CS5001 React JS
+                            </h5>
+                            <p className="wd-dashboard-course-title">
+                            Intensive Foundations of CS
+                            </p>
+                            <button> Go </button>
+                        </div>
+                    </Link>
+                </div>
+                <div className="wd-dashboard-course">
+                    <Link className="wd-dashboard-course-link"
+                        to="/Kanbas/Courses/1234/Home">
+                        <img src="/images/reactjs.jpg" width={200} />
+                        <div>
+                            <h5>
+                                CS5004 React JS
+                            </h5>
+                            <p className="wd-dashboard-course-title">
+                            Object-Oriented Design
+                            </p>
+                            <button> Go </button>
+                        </div>
+                    </Link>
+                </div>
+                <div className="wd-dashboard-course">
+                    <Link className="wd-dashboard-course-link"
+                        to="/Kanbas/Courses/1234/Home">
+                        <img src="/images/reactjs.jpg" width={200} />
+                        <div>
+                            <h5>
+                                CS5008 React JS
+                            </h5>
+                            <p className="wd-dashboard-course-title">
+                                Data Structure and Algorithm
+                            </p>
+                            <button> Go </button>
+                        </div>
+                    </Link>
+                </div>
+                <div className="wd-dashboard-course">
+                    <Link className="wd-dashboard-course-link"
+                        to="/Kanbas/Courses/1234/Home">
+                        <img src="/images/reactjs.jpg" width={200} />
+                        <div>
+                            <h5>
+                                CS5500 React JS
+                            </h5>
+                            <p className="wd-dashboard-course-title">
+                                Found of Software Engineering
+                            </p>
+                            <button> Go </button>
+                        </div>
+                    </Link>
+                </div>
+                <div className="wd-dashboard-course">
+                    <Link className="wd-dashboard-course-link"
+                        to="/Kanbas/Courses/1234/Home">
+                        <img src="/images/reactjs.jpg" width={200} />
+                        <div>
+                            <h5>
+                                CS5800 React JS
+                            </h5>
+                            <p className="wd-dashboard-course-title">
+                                Algorithm
+                            </p>
+                            <button> Go </button>
+                        </div>
+                    </Link>
+                </div>
+                <div className="wd-dashboard-course">
+                    <Link className="wd-dashboard-course-link"
+                        to="/Kanbas/Courses/1234/Home">
+                        <img src="/images/reactjs.jpg" width={200} />
+                        <div>
+                            <h5>
+                                CS5700 React JS
+                            </h5>
+                            <p className="wd-dashboard-course-title">
+                                Computer Networks
+                            </p>
+                            <button> Go </button>
+                        </div>
+                    </Link>
+                </div>
+                <div className="wd-dashboard-course"> ... </div>
+                <div className="wd-dashboard-course"> ... </div>
+            </div>
+        </div>
+    );
+}
