8acb5aba301f5775126927c9dac05e58df38f376
Rolin128 <li.yuchen@northeastern.edu>
2024-10-17 18:41:21 -0700

    a3-updates

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -34,7 +34,7 @@ export default function TOC() {
                 </a>
             </li>
             <li className="nav-item">
-                <a id="wd-k" href="https://github.com/Rolin128/kanbas-react-web-app" className="nav-link">
+                <a id="wd-github" href="https://github.com/Rolin128/kanbas-react-web-app" className="nav-link">
                     My GitHub
                 </a>
             </li>
